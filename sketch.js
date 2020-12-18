5dcd72f582d03876327e45ca3cf0d9b2b0dac53e
kasinath777 <74859052+kasinath777@users.noreply.github.com>
2020-12-18 21:07:09 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,39 +3,47 @@ var wall1;
 var speed,weight;
 
 function setup() {
-  createCanvas(400,200);
- car1=createSprite(50,100,50,50);
- car1.velocityX=speed;
- 
-
- wall1 =createSprite(390,100, 50,height/2);
- 
-
- speed=random(55,90);
+  createCanvas(1600,400);
+  speed=random(55,90);
  weight=random(400,1500);
 
+
+  
+ car=createSprite(50,200,50,50);
+ car.velocityX=speed;
+  car.shapeColor=color(255)
+ 
+
+ wall =createSprite(1500,200, 60,height/2);
+  wall.shapeColor=color(80,80,80)
 }
+ 
+
+ 
 
 function draw() {
   background(255,255,255); 
   
-  if(wall1.x-car1.x < (car.width+wall1.width)/2)
+   if(wall.x-car.x < (car.width+wall.width)/2)
   {
-    car1.velocityX=0;
-  var deformation=0.5 * weight * speed * speed/22509;
-  if(deformation>180)
-  {
-    car1.shapeColor=color(255,0,0)
+  	car.velocityX=0;
+  	var deformation=0.5 * weight * speed *speed/22500;
+	if(deformation>180)
+	{
+		car.shapeColor=color(255,0,0);
+	}
 
-  }
-  if(deformation<180 && deformation>100)
-  {
-    car1.shapeColor=color(230,230,0);
-  }
-  if(deformation<100)
-  {
-    car1.shapeColor=color(0,255,0)
-  }
-  }
+	if(deformation<180 && deformation>100)
+	{
+		car.shapeColor=color(230,230,0);
+	}
+
+	if(deformation<100)
+	{
+		car.shapeColor=color(0,255,0);
+	}
+  }  
+  
+  
   drawSprites();
 }
